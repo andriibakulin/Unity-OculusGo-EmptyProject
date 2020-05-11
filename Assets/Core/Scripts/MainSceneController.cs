1e4c9bded4fe7befeacf124e09ae90528780e2a7
Andrii Bakulin <andrei.bakulin@gmail.com>
2020-05-11 14:57:16 +0700

    Base version

---
+++ Assets/Core/Scripts/MainSceneController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Duksel.Helpers;
+
+public class MainSceneController : MonoBehaviour
+{
+	void Update()
+	{
+		if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger))
+		{
+            FPS.ShowMessage("Trigger down");
+		}
+        else if (OVRInput.GetUp(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            FPS.ClearMessage();
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.Back))
+		{
+            FPS.ShowMessage("Back button down");
+		}
+        else if (OVRInput.GetUp(OVRInput.Button.Back))
+        {
+            FPS.ClearMessage();
+        }
+
+        if (OVRInput.GetDown(OVRInput.Button.PrimaryTouchpad))
+		{
+            FPS.ShowMessage("PrimaryTouchpad down");
+		}
+        else if (OVRInput.Get(OVRInput.Button.PrimaryTouchpad))
+        {
+            float x = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad).x;
+            float y = OVRInput.Get(OVRInput.Axis2D.PrimaryTouchpad).y;
+
+            FPS.ShowMessage("Touchpad [" + x.ToString("F2") + "x" + y.ToString("F2") + "]");
+        }
+        else if (OVRInput.GetUp(OVRInput.Button.PrimaryTouchpad))
+        {
+            FPS.ClearMessage();
+        }
+	}
+}
---
+++ Assets/CoreDummy/DukselHelpers/FPS/FPS.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Duksel.Helpers
+{
+    public class FPS : MonoBehaviour
+    {
+        [Header("Text Mesh Label")]
+
+        [SerializeField]
+        TextMesh labelFps;
+
+        [SerializeField]
+        TextMesh labelMessage;
+
+        static FPS instance;
+
+        int FrameCounter;
+        int FramePerSecondLast;
+        string FramePerSecondString;
+
+        void Awake()
+        {
+            instance = this;
+            ClearMessage();
+        }
+
+        void OnDestroy()
+        {
+            instance = null;
+        }
+
+        void Start()
+        {
+            InvokeRepeating("TickFpsFixer", 0f, 1f);
+        }
+
+        void Update()
+        {
+            FrameCounter++;
+        }
+
+        void TickFpsFixer()
+        {
+            FramePerSecondLast = FrameCounter;
+            FramePerSecondString = FramePerSecondLast.ToString() + " fps";
+            FrameCounter = 0;
+
+            if (labelFps != null)
+                labelFps.text = FramePerSecondString;
+        }
+
+        public static void ShowMessage(string message)
+        {
+            if (instance != null && instance.labelMessage != null)
+                instance.labelMessage.text = message;
+
+            Debug.Log("FPS: " + message);
+        }
+
+        public static void ClearMessage()
+        {
+            ShowMessage("");
+        }
+    }
+}
